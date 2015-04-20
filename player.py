4fc1d0b312de4c3cce86c38bfb3812baf3b6a3cd
knifeofdreams <knifeofdreams86@gmail.com>
2015-04-20 20:02:24 +0200

    Raise when ace pair

--- player.py
+++ player.py
@@ -1,9 +1,11 @@
-
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 100
+        if game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'A' and game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'A':
+            return 1000
+        else:
+            return 100
 
     def showdown(self, game_state):
         pass
--- player_test.py
+++ player_test.py
@@ -1,13 +1,25 @@
 import unittest
+import json
+from pprint import pprint
 
 from player import Player
 
 class PlayerTest(unittest.TestCase):
     def setUp(self):
         self.player = Player()
+        self.game_state = {}
+
+        with open('gamestate.json') as json_file:
+            self.game_state = json.load(json_file)
 
     def test_bet_request_should_return_integer(self):
-        self.assertIsInstance(self.player.betRequest({}), int)
+        self.assertIsInstance(self.player.betRequest(self.game_state), int)
+
+    def test_bet_1000_when_ace_pair(self):
+        with open('gamestate.json') as json_file:
+            game_state = json.load(json_file)
+        print game_state['players'][game_state['in_action']]['hole_cards'][0]['rank']
+        self.assertEqual(self.player.betRequest(self.game_state), 1000)
 
 if __name__ == '__main__':
     unittest.main()
