9d13fc2c2474f4ba4a9437d728c12e65dcab3375
knifeofdreams <knifeofdreams86@gmail.com>
2015-04-20 18:45:51 +0200

    Test to return integer

--- player_test.py
+++ player_test.py
@@ -7,8 +7,7 @@ class PlayerTest(unittest.TestCase):
         self.player = Player()
 
     def test_bet_request_should_return_integer(self):
-        # self.assertIsInstance(player.betRequest({}), int)
-        self.assertTrue(True)
+        self.assertIsInstance(self.player.betRequest({}), int)
 
 if __name__ == '__main__':
     unittest.main()
