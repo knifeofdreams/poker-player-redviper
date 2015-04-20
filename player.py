f89770a7a628196fb13edebd5405614f73a4910f
knifeofdreams <knifeofdreams86@gmail.com>
2015-04-20 20:17:32 +0200

    Yolo viper

--- player.py
+++ player.py
@@ -1,8 +1,23 @@
 class Player:
-    VERSION = "Such viper"
+    VERSION = "YOLO VIPER"
 
     def betRequest(self, game_state):
-        if game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'A' and game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'A':
+
+        ONE_HIGH_CARD = game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'A' or \
+                        game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'Q' or \
+                        game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'K' or \
+                        game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'J'
+
+        OTHER_HIGH_CARD = game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'A' or \
+                          game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'Q' or \
+                          game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'K' or \
+                          game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'J'
+
+        ACE_PAIR = game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'A' and \
+                   game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'A'
+        if ACE_PAIR:
+            return game_state['players'][game_state['in_action']]['stack']
+        elif ONE_HIGH_CARD:
             return game_state['players'][game_state['in_action']]['stack']/6
         else:
             return 100
--- player_test.py
+++ player_test.py
@@ -18,7 +18,7 @@ class PlayerTest(unittest.TestCase):
         with open('gamestate.json') as json_file:
             game_state = json.load(json_file)
         print game_state['players'][game_state['in_action']]['hole_cards'][0]['rank']
-        self.assertEqual(self.player.betRequest(self.game_state), 265)
+        self.assertEqual(self.player.betRequest(self.game_state), 1590)
 
 if __name__ == '__main__':
     unittest.main()
